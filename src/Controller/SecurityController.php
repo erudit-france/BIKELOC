422345c81c98a0cd43f6a3a9833eb5edb5c0c046
TomTom <tom_deprez@hotmail.fr>
2021-01-24 11:31:20 +0100

    V1

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+use LogicException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class SecurityController extends AbstractController implements AuthenticatorInterface
+{
+
+
+    private $userRepository;
+
+    public function __construct(userRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $requete, PersistenceManagerRegistry $managerRegistry)
+    {
+        // $user = new User();
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $pseudo = $user->getUsername();
+        //     $email = $user->getEmail();
+        //     dump($pseudo);
+        //     dump($email);
+        //     $user->setRoles(['ROLE_USER']);
+        //     $user->setPassword($user->algoCryptage($user->getPassword()));
+        //     $manager = $managerRegistry->getManager();
+        //     $manager->persist($user);
+        //     $manager->flush();
+        //     $this->get('session')->getFlashBag()->add('succès', 'Compte créé!');
+        //     return $this->redirectToRoute('connexionGuide');
+        // }
+        return $this->render('security/inscription.html.twig');
+    }
+
+    
+    /**
+     * @Route("/inscriptionUser", name="inscriptionUser", methods={"POST"})
+     */
+    public function inscriptionUser()
+    {
+    }
+
+    /**
+     * @Route("/logout", name="logout", methods={"GET"})
+     */
+    public function logout()
+    {
+    }
+
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function connexion(SessionInterface $session, $_route)
+    {
+        // $user = new User();
+        // $form = $this->createFormBuilder($user)
+        // ->add('email')
+        // ->add('password', PasswordType::class)
+        // ->add('Connexion', SubmitType::class, [
+        //     'attr' => [
+        //         'class' => 'btn btn-primary'
+        //     ]
+        // ])
+        // ->getForm();
+        // $form->handleRequest($requete);
+
+        return $this->render(
+            'guide/connexion.html.twig'
+        );
+    }
+    /**
+     * Does the authenticator support the given Request?
+     *
+     * If this returns false, the authenticator will be skipped.
+     *
+     * Returning null means authenticate() can be called lazily when accessing the token storage.
+     */
+    public function supports(Request $request): ?bool
+    {
+        // dump($request->attributes->get('_route') === 'connexion' && $request->isMethod('POST'));
+        return $request->attributes->get('_route') === 'connexion' && $request->isMethod('POST');
+    }
+
+    /**
+     * Create a passport for the current request.
+     *
+     * The passport contains the user, credentials and any additional information
+     * that has to be checked by the Symfony Security system. For example, a login
+     * form authenticator will probably return a passport containing the user, the
+     * presented password and the CSRF token value.
+     *
+     * You may throw any AuthenticationException in this method in case of erreur (e.g.
+     * a UsernameNotFoundException when the user cannot be found).
+     *
+     * @throws AuthenticationException
+     */
+    public function authenticate(Request $request): PassportInterface
+    {
+        // find a user based on an "email" form field
+        // dump($this->userRepository);
+        $user = $this->userRepository->findOneByEmail($request->request->get('username'));
+        $request->getSession()->set('back_username', $request->request->get('username'));
+
+        if (!$user) {
+            throw new UsernameNotFoundException();
+        }
+
+        return new Passport($user, new PasswordCredentials($request->request->get('password')), [
+            // and CSRF protection using a "csrf_token" field
+            new CsrfTokenBadge('login', $request->request->get('csrf_token')),
+            // new RememberMeBadge
+        ]);
+    }
+    /**
+     * Shortcut to create a PostAuthenticationToken for you, if you don't really
+     * care about which authenticated token you're using.
+     *
+     * @return PostAuthenticationToken
+     */
+    public function createAuthenticatedToken(PassportInterface $passport, string $firewallName): TokenInterface
+    {
+        if (!$passport instanceof UserPassportInterface) {
+            throw new LogicException(sprintf('Passport does not contain a user, overwrite "createAuthenticatedToken()" in "%s" to create a custom authenticated token.', \get_class($this)));
+        }
+
+        return new PostAuthenticationToken($passport->getUser(), $firewallName, $passport->getUser()->getRoles());
+    }
+
+    /**
+     * Called when authentication executed and was succèssful!
+     *
+     * This should return the Response sent back to the user, like a
+     * RedirectResponse to the last page they visited.
+     *
+     * If you return null, the current request will continue, and the user
+     * will be authenticated. This makes sense, for example, with an API.
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        $sesion = new Session();
+        $sesion = $sesion->get('lastUrl');
+        $this->get('session')->getFlashBag()->add('succès', 'Connexion réussie!');
+
+        if (isset($sesion['route'])) {
+            if (isset($sesion['id'])) {
+                if ($sesion['route'] == "Category") {
+                    return $this->redirectToRoute($sesion['route'], [
+                        'category' => $sesion['category'],
+                        'id' => $sesion['id']
+                    ]);
+                }
+                if ($sesion['route'] == "Search") {
+                    return $this->redirectToRoute($sesion['route'], [
+                        'search' => $sesion['search'],
+                        'id' => $sesion['id']
+                    ]);
+                }
+                return $this->redirectToRoute($sesion['route'], [
+                    'id' => $sesion['id']
+                ]);
+            }
+            return $this->redirectToRoute($sesion['route']);
+        }
+        return null;
+    }
+
+    /**
+     * Called when authentication executed, but failed (e.g. wrong username password).
+     *
+     * This should return the Response sent back to the user, like a
+     * RedirectResponse to the login page or a 403 response.
+     *
+     * If you return null, the request will continue, but the user will
+     * not be authenticated. This is probably not what you want to do.
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $this->get('session')->getFlashBag()->add('erreur', 'Informations invalide!');
+        return $this->redirectToRoute('connexion');
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $roles = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function algoCryptage($string)
+    {
+        return password_hash($string, PASSWORD_DEFAULT);
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    public function load(User $user, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $user->setPassword($passwordEncoder->encodePassword(
+            $user,
+            $user->getPassword()
+        ));
+    }
+}
